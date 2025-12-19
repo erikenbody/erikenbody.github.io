9683eccf054f7efa2f80a9904f06fa3c788b3bc2
erikenbody <eenbody@tulane.edu>
2025-12-19 10:19:57 -0500

    Update base path for root domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/enbodylab.github.io/', // GitHub Pages project site base path
+  base: '/', // Root domain for erikenbody.github.io
 })
